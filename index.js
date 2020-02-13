a550e1de1d7ffe6216b02b293b02059d45597cb4
Ana Berrocal <anaberrocal@ymail.com>
2020-02-13 16:43:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ console.log(string.toLowerCase())
 
 function sayHiToGrandma(string) {
     if(string.toLowerCase() == string) {
-    return ("I can't hear you!")
+    return "I can't hear you!"
   }
 };
 
 function sayHiToGrandma(string) {
   if(string.toUpperCase() === string) {
-    return ("YES INDEED!")
+    return "YES INDEED!"
   }
 };
 
